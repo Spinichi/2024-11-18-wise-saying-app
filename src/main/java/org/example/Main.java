a2513bc2ebe0f7923be60c5cfed4648486e84096
Spinichi <spinichi@naver.com>
2024-11-20 00:30:51 +0900

    Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,6 +21,8 @@ package org.example;
 //    }
 //}
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -33,6 +35,17 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력된 명령어 : %s".formatted(cmd));
+        // 문자열 s int형 d
+
+        scanner.close();
+
+
     }
 }
